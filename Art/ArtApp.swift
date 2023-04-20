be2489235ff00cb9e860ae0878062d84478a2cea
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-04-20 10:13:30 -0400

    Initial Commit

---
+++ Art/ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtApp.swift
+//  Art
+//
+//  Created by Evelyn Chen on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
